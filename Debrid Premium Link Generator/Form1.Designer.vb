2e57e570df519ef16809a33d8b14b8abd0cd67fc
iamjbenas <99813093+iamjbenas@users.noreply.github.com>
2024-04-17 17:08:53 +0800

    Add files via upload

---
+++ Debrid Premium Link Generator/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtUserInputLink = New System.Windows.Forms.TextBox()
+        Me.txtPremiumLink = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 14)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(80, 63)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'txtUserInputLink
+        '
+        Me.txtUserInputLink.Location = New System.Drawing.Point(96, 30)
+        Me.txtUserInputLink.Name = "txtUserInputLink"
+        Me.txtUserInputLink.Size = New System.Drawing.Size(371, 20)
+        Me.txtUserInputLink.TabIndex = 10
+        '
+        'txtPremiumLink
+        '
+        Me.txtPremiumLink.Location = New System.Drawing.Point(96, 56)
+        Me.txtPremiumLink.Name = "txtPremiumLink"
+        Me.txtPremiumLink.Size = New System.Drawing.Size(258, 20)
+        Me.txtPremiumLink.TabIndex = 11
+        Me.txtPremiumLink.Text = "Generated Link"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(93, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 17)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Enter link:"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI Semibold", 8.75!, System.Drawing.FontStyle.Bold)
+        Me.Button1.Location = New System.Drawing.Point(356, 53)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(111, 25)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Generate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(93, 88)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 17)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Filename:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(480, 123)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPremiumLink)
+        Me.Controls.Add(Me.txtUserInputLink)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Simple Debrid Premium Link Generator - Josh"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtUserInputLink As TextBox
+    Friend WithEvents txtPremiumLink As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Debrid Premium Link Generator/Form1.vb
@@ -0,0 +1,87 @@
+﻿Imports System.Net
+Imports System.IO
+Imports Newtonsoft.Json.Linq
+Imports System.Net.Http
+Imports System.Threading
+Imports Microsoft.Win32
+Imports System.ComponentModel
+Imports Newtonsoft.Json
+
+Public Class Form1
+
+    '////////////////////////////////////////////////////////////////////////////
+    '// A proper credit will do, made by JOSH.                                ///
+    '// Purchase API key here > https://debrid-link.com/                     ///
+    '// You can find the supporting websites as well on their page.         ///
+    '// it's a simple GUI for Debrid-Link, just incase you want to create   ///
+    '// your own Premium Link Generator                                    ///
+    '////////////////////////////////////////////////////////////////////////
+
+    ''DON'T FORGET TO ADD JSON PACKAGE FROM NUGET TO AVOID ERROR!
+
+    Dim WithEvents webClient As WebClient
+    Dim totalBytes As Long
+    Dim downloadedBytes As Long
+    Dim lastBytesDownloaded As Long
+    Dim downloadStartTime As DateTime
+    Public Class DownloadInfo
+        Public Property Id As String
+        Public Property Filename As String
+        Public Property DownloadUrl As String
+        Public Property FileSize As Long
+    End Class
+
+    Private Async Function Button1_ClickAsync(sender As Object, e As EventArgs) As Task Handles Button1.Click
+        Dim url As String = txtUserInputLink.Text
+        Dim downloadInfo As DownloadInfo = Await GetDownloadUrlAsync(url)
+        If downloadInfo IsNot Nothing Then
+            txtPremiumLink.Text = downloadInfo.DownloadUrl
+            Label2.Text = "Filename: " & vbNewLine & downloadInfo.Filename
+        Else
+        End If
+    End Function
+    Private Async Function GetDownloadUrlAsync(url As String) As Task(Of DownloadInfo)
+        Dim downloadInfo As DownloadInfo = Nothing
+
+        Try
+            Dim request As HttpWebRequest = CType(WebRequest.Create("https://debrid-link.com/api/v2/downloader/add"), HttpWebRequest)
+            request.Method = "POST"
+            request.ContentType = "application/json"
+            request.Headers.Add("Authorization", "Bearer YOUR API HERE")
+            request.Accept = "application/json"
+
+            Dim jsonData As String = "{ ""url"": """ & url & """ }"
+
+            Dim byteArray As Byte() = System.Text.Encoding.UTF8.GetBytes(jsonData)
+            request.ContentLength = byteArray.Length
+
+            Using dataStream As Stream = Await request.GetRequestStreamAsync()
+                Await dataStream.WriteAsync(byteArray, 0, byteArray.Length)
+            End Using
+
+            Dim response As HttpWebResponse = CType(Await request.GetResponseAsync(), HttpWebResponse)
+
+            Using reader As New StreamReader(response.GetResponseStream())
+                Dim jsonResponse As String = Await reader.ReadToEndAsync()
+
+                Dim jsonObject As JObject = JObject.Parse(jsonResponse)
+                downloadInfo = New DownloadInfo With {
+                .Id = jsonObject("value")("id").ToString(),
+                .Filename = jsonObject("value")("name").ToString(),
+                .DownloadUrl = jsonObject("value")("downloadUrl").ToString(),
+                .FileSize = jsonObject("value")("size").ToObject(Of Long)()
+            }
+            End Using
+
+        Catch ex As Exception
+
+        End Try
+
+        Return downloadInfo
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+End Class
